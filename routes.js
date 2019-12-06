06b96d8f56a9bd68dcbfb26e7f6aac7cdfef2aa3
Luis Manuel Brito <lsbrt4@gmail.com>
2019-12-06 15:18:38 -0430

    agrego componentes de routes y server del otro proyecto para el deploy en now y probar

---
+++ routes.js
@@ -0,0 +1,8 @@
+const routes = module.exports = require('next-routes')()
+
+routes
+.add('index', '/')
+.add('home')
+.add('bicameral')
+/* .add('channel', '/:slug.:id', 'channel')
+.add('podcast', '/:slugChannel.:idChannel/:slug.:id', 'podcast') */
